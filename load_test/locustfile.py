296a9ee015b705a1fd89ab9793ffd618ffca7bd8
Mahdi Gheidi <mahdi.gheidi@cafebazaar.ir>
2023-05-27 21:40:00 +0330

    add all endpoints for locust test

--- load_test/locustfile.py
+++ load_test/locustfile.py
@@ -1,6 +1,9 @@
 from locust import HttpUser, task
 
-class HelloWorldUser(HttpUser):
+class BackendLoadTest(HttpUser):
     @task
-    def hello_world(self):
+    def load_test(self):
         self.client.get("/auth/req_pq", verify=False)
+        self.client.get("/auth/req_dh_params", verify=False)
+        self.client.get("/biz/get_users", verify=False)
+        self.client.get("/biz/get_users_with_sql_injection", verify=False)
